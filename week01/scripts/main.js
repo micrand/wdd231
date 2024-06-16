01f3863d5b5962888a4654eb29f495e6deaa7613
Mickael <micrandr@gmail.com>
2024-06-16 09:08:56 +0300

    W01 - Homepage chages

--- week01/scripts/main.js
+++ week01/scripts/main.js
@@ -7,3 +7,13 @@ let lastModifiedDate = document.lastModified;
 outputFullYear.innerHTML = `${currentYear}`;
 outputLastModified.innerHTML = `${lastModifiedDate}`;
 console.log(lastModifiedDate);
+
+
+
+const mainNav = document.querySelector('.navigation');
+const hamburgerButton = document.querySelector('#menu');
+
+hamburgerButton.addEventListener( 'click', () => {
+    mainNav.classList.toggle('show');
+    hamburgerButton.classList.toggle('show');
+});
