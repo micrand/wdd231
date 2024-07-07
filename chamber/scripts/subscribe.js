4a187a9fc8dbe546cbf84265e6730351f2597189
Mickael <micrandr@gmail.com>
2024-07-07 09:25:47 +0300

    W04 - Form join changes

--- chamber/scripts/subscribe.js
+++ chamber/scripts/subscribe.js
@@ -17,3 +17,24 @@ let formData = document.getElementById('form-subscription-data');
 const subscriptionDataUrl = location.href;
 
 console.log(subscriptionDataUrl);
+
+
+
+/* Handle Benefits modal */
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+function ShowModal() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
